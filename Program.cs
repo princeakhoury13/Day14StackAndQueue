f901218e33752f1de2f678c1de70095f3dbcfab6
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-25 22:51:42 +0530

    added: Welcome to master branch

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day14StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 14 --> Stacks and Queue");
+        }
+    }
+}
