987439022aa101418f350ecd1d6473142bc26a5a
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-25 22:59:52 +0530

    added: Create a queue and add elements

--- Program.cs
+++ Program.cs
@@ -1,26 +1,17 @@
-﻿namespace Day14StackAndQueue
+﻿using System.Collections;
+
+namespace Day14StackAndQueue
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Stack <int> stack  = new Stack<int>();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
+            Queue <int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
 
-            foreach(int ele in stack)
-            {
-                Console.WriteLine(ele);
-            }
-
-
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("Top element : "+stack.Peek());
-            Console.WriteLine("------------------------------");
-            stack.Pop();
-
-            foreach (int ele in stack)
+            foreach(int ele in queue)
             {
                 Console.WriteLine(ele);
             }
